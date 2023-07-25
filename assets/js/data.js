84c63c49659ddd8543566b0f6b376230e5a41ea4
Farhan raj Najmul <109592222+dev-najmul@users.noreply.github.com>
2023-07-25 19:05:37 +0600

    add fb design

---
+++ assets/js/data.js
@@ -0,0 +1,44 @@
+const fbPost = [
+  {
+    auth_name: "Farhan raj Najmul",
+    auth_photo:
+      "https://scontent.fbzl5-1.fna.fbcdn.net/v/t1.6435-9/151705337_253872116341697_2102967531897629567_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFD9ieeWlwnImJ2sD6GrgF9EIQrqTu5e3kQhCupO7l7eSjgBaTUXH-xeR3IxQufpc-3f80FTa9FE-trc8-tZzG3&_nc_ohc=vlALeVKPwZYAX_8gBAB&_nc_ht=scontent.fbzl5-1.fna&oh=00_AfBCjtruiDQmbN6H_c4i_pCKLZK_ULtNYLWW2e8evuKpCg&oe=64E6BDF8",
+
+    post_content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit optio necessitatibus id nemo iste quod?",
+    post_photo:
+      "https://embedsocial.com/wp-content/uploads/2020/10/add-links-instagram-posts.jpg",
+
+    post_like: 500,
+    post_comment: 200,
+    post_time: "1 day",
+  },
+  {
+    auth_name: "Elon Mask",
+    auth_photo:
+      "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+
+    post_content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit optio necessitatibus id nemo iste quod?",
+    post_photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNcA1w-Mf8L5Rq03F0-LHV5fciE5JN2GUeUw&usqp=CAU",
+
+    post_like: 1500,
+    post_comment: 752,
+    post_time: "50 min",
+  },
+  {
+    auth_name: "Mark Zuckerberg",
+    auth_photo:
+      "https://assets.entrepreneur.com/content/3x2/2000/1652298519-GettyImages-13276855511.jpg",
+
+    post_content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit optio necessitatibus id nemo iste quod?",
+    post_photo:
+      "https://c.ndtvimg.com/2022-09/ibd0i4so_mark-zuckerberg-and-priscilla-chan-are-expecting-a-baby-girl_625x300_22_September_22.jpg?im=Resize=(1230,900)",
+
+    post_like: 7800,
+    post_comment: 1252,
+    post_time: "5 min",
+  },
+];
